151a00c0f21e69e662ec396ebb7ced569e6aa029
alt-potato <jason.why.zhuang@gmail.com>
2026-02-04 01:30:48 -0500

    Fix outer rim overwriting sciences

--- data-updates.lua
+++ data-updates.lua
@@ -1,6 +1,6 @@
-if mods["Krastorio2"] or mods["Krastorio2-spaced-out"] then
-    -- fighting Krastorio2 for modifications to the base sciences :|
-    require("lib.icon-manipulation").apply_icon({ "tool" })
+if mods["Krastorio2"] or mods["Krastorio2-spaced-out"] or mods["outer-rim"] then
+	-- fighting Krastorio2 et al. for modifications to the base sciences :|
+	require("lib.icon-manipulation").apply_icon({ "tool" })
 end
 
 require("lib.icon-manipulation").apply_icon({ "recipe", "technology" })
